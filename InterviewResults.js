1223a19da28af7aa4b2e1dca75f6756ea4693e7f
Andrew McConnell <andrewmcconnell2016@u.northwestern.edu>
2017-03-07 14:36:41 -0600

    Update InterviewResults.js

--- InterviewResults.js
+++ InterviewResults.js
@@ -1,4 +1,4 @@
 var locate = window.location.search.toString();
 var time = locate.substring(locate.indexOf("=") + 1);
-document.getElementById('time').innerHTML=time;
+if(time) document.getElementById('time').innerHTML=time;
 console.log(time);
