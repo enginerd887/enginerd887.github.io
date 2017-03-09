b81e72bd53c17fe5641676f80bd3eff237a128bb
Daniel Perlovsky <Daniel Perlovsky>
2017-03-09 12:23:16 -0600

    Ability to Name Recording

--- InterviewResults.js
+++ InterviewResults.js
@@ -2,3 +2,15 @@ var locate = window.location.search.toString();
 var time = locate.substring(locate.indexOf("=") + 1);
 if(time) document.getElementById('time').innerHTML=time;
 console.log(time);
+
+var nameForm = document.getElementById("nameForm");
+var recordingTitle = document.getElementById("recordingTitle");
+recordingTitle.style.visibility = "hidden";
+
+nameForm.onsubmit = function() {
+	recordingTitle.innerHTML = document.getElementById("name").value;
+
+	recordingTitle.style.visibility = "visible";
+	nameForm.style.visibility = "hidden";
+	return false;
+}
